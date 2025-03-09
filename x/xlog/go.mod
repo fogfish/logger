d4f6545514d74c5973b3dffb5288560ff89c48b3
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2025-03-09 19:18:45 +0200

    slog extension

---
+++ x/xlog/go.mod
@@ -0,0 +1,5 @@
+module github.com/fogfish/logger/x/xlog
+
+go 1.23.1
+
+require github.com/fogfish/logger/v3 v3.2.0
---
+++ x/xlog/version.go
@@ -0,0 +1,11 @@
+//
+// Copyright (C) 2021 - 2025 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// https://github.com/fogfish/logger
+//
+
+package xlog
+
+const Version = "x/xlog/v0.0.1"
